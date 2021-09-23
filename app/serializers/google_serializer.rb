3617bb1eb7ee4e9e1ab82be7e0019907a7ddadc5
Tom Evans <tom@sovi.fm>
2021-09-23 11:44:47 -0400

    tests: thunder_client collection update

--- app/serializers/google_serializer.rb
+++ app/serializers/google_serializer.rb
@@ -3,4 +3,19 @@
 module GoogleSerializer
   MandatoryField = Class.new(StandardError)
   MalformedField = Class.new(StandardError)
+  
+  def follow(url, redirects = 5, &block)
+    raise RuntimeError, "Too many redirects" if redirects == 0
+    uri = URI.parse(url)
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      yield(url)
+      request = Net::HTTP::Head.new(uri.request_uri)
+      response = http.request(request)
+      case response
+      when Net::HTTPRedirection
+        follow(response['location'], redirects - 1, &block)
+      end
+    end
+  end
+
 end
